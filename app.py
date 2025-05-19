5573c9b9e1932f9a8c090b27a30b439fcbefc41f
Miguelangel Rendón <silvermiguel96@gmail.com>
2025-05-19 18:07:15 -0500

    Refactor loan application API: update routes, enhance error handling, and add Swagger documentation

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flasgger import Swagger
 from interfaces.routes import bp
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.register_blueprint(bp)
+CORS(app)
+
+swagger = Swagger(app)
+
+app.register_blueprint(bp, url_prefix='/api/v1')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
--- infrastructure/validators.py
+++ infrastructure/validators.py
@@ -1,6 +1,6 @@
 from marshmallow import Schema, fields, ValidationError
 
 class LoanApplicationSchema(Schema):
-    taxId = fields.String(required=True)
-    businessName = fields.String(required=True)
-    requestedAmount = fields.Float(required=True)
+    taxId = fields.String(required=True, data_key="tax_id")
+    businessName = fields.String(required=True, data_key="business_name")
+    requestedAmount = fields.Float(required=True, data_key="requested_amount")
